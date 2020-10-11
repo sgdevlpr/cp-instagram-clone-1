7f008a2f4bde39f3da8e3f6c405ff3c1d092f262
Patryk <patryk@Mac-mini-Patryk.local>
2020-10-11 14:54:39 +0200

    Updates

---
+++ precache-manifest.e5950aa7aa137a0c9343d2c447e85e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f434ea236f7524b037d59c78515064",
+    "url": "/cp-instagram-clone/index.html"
+  },
+  {
+    "revision": "40bf44974054d8b3d5b7",
+    "url": "/cp-instagram-clone/static/css/main.d54d43be.chunk.css"
+  },
+  {
+    "revision": "134fceb1af27822f0062",
+    "url": "/cp-instagram-clone/static/js/2.268f6a0b.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/cp-instagram-clone/static/js/2.268f6a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40bf44974054d8b3d5b7",
+    "url": "/cp-instagram-clone/static/js/main.5360cee2.chunk.js"
+  },
+  {
+    "revision": "b51633c0e1fa58ecdf11",
+    "url": "/cp-instagram-clone/static/js/runtime-main.264bf094.js"
+  }
+]);
